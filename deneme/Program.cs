77dd20ea1aa0a82f8f282141ecb5e280885afa4e
Derviş Keleş <dervis.keles@gmail.com>
2020-02-07 18:06:59 +0300

    Initial Commit

---
+++ deneme/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deneme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("http://deneme.ticimax.com.tr/api/GetProduct");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("application/json", "{ \"TicimaxApiKey\" : \"34243241\", \"TicimaxApiSecret\" : \"24324324\" }", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+
+        }
+    }
+}
